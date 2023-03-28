23d0fadb170eda7dec3db3a0817536f0a55177cb
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-03-28 10:30:41 +0200

    285. Fixing a Bug with the Resolver

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,9 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {map, tap} from "rxjs";
 
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
-import {map} from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -16,10 +16,19 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>('https://ng-course-recipe-book-d7f4c-default-rtdb.firebaseio.com/recipes.json')
-      .pipe(map(recipes => recipes.map( recipe => {
-        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-      })))
-      .subscribe(recipes => this.recipeService.setRecipes(recipes));
+    return this.http.get<Recipe[]>('https://ng-course-recipe-book-d7f4c-default-rtdb.firebaseio.com/recipes.json')
+      .pipe(
+        map(recipes => {
+          return recipes.map( recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          })
+        }),
+        tap( recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      )
   }
 }
