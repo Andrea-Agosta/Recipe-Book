4db9ba3213de9d0dcaab3d6a8edba6866b97bad1
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-03-29 09:26:37 +0200

    316. Data Binding & Event Binding

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,6 @@
 import {Component, ComponentFactoryResolver, ViewChild} from "@angular/core";
 import {NgForm} from "@angular/forms";
-import {Observable} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 
 import { AuthService, AuthResponseData } from "./auth.service";
 import {Router} from "@angular/router";
@@ -11,11 +11,12 @@ import {PlaceholderDirective} from "../shared/placeholder/placeholder.directive"
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements onDestroy {
   isLoginMode: boolean = true;
   isLoading: boolean = false;
   error: string = null;
   @ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
+  private closeSub: Subscription;
 
   constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) {}
 
@@ -59,6 +60,17 @@ export class AuthComponent {
     const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
     const hostViewContainerRef = this.alertHost.viewContainerRef;
     hostViewContainerRef.clear();
-    hostViewContainerRef.createComponent(alertCmpFactory);
+    const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
   }
 }
