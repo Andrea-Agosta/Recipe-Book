d3615bbab020040306eb49cabe65b8924d9f19b7
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-04-03 16:37:31 +0200

    390. Getting Started with NgRx for Recipes

---
+++ src/app/recipes/store/recipe.reducer.ts
@@ -0,0 +1,20 @@
+import {Recipe} from "../recipe.model";
+
+import * as RecipesActions from "./recipe.actions";
+
+export interface State {
+  recipes: Recipe[];
+}
+
+const initialState: State = {
+  recipes: []
+}
+
+export const RecipeReducer = (state: State = initialState, action: RecipesActions.RecipesActions) => {
+  switch (action.type) {
+    case RecipesActions.SET_RECIPES:
+     return {...state, recipes: [...action.payload]};
+    default:
+      return state;
+  }
+}
